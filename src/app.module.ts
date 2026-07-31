1b4a6d1656c20e376e594629457f60426b25a956
dohun03 <dlehgns0217@gmail.com>
2026-07-31 18:46:18 +0900

    feat(api): ThrottlerGuard 기반 검색 및 API 연속 요청 제한 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { DatabaseModule } from './database/database.module';
 import { TrendsModule } from 'trends/trends.module';
 
 @Module({
@@ -12,8 +14,22 @@ import { TrendsModule } from 'trends/trends.module';
       isGlobal: true, 
       envFilePath: '.env',
     }),
+    // 10초에 10번으로 요청 제한
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10000,
+        limit: 10,
+      },
+    ]),
     DatabaseModule,
     TrendsModule,
+  ],
+  providers: [
+    // 모든 컨트롤러에 Throttler 자동 적용
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class AppModule {}
