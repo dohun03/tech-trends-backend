3ba1e1e556cf159ae7762aa14dd6ed86a793bfd0
dohun03 <dlehgns0217@gmail.com>
2026-08-14 16:39:43 +0900

    chore: BullMQ 패키지 설치 및 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { DatabaseModule } from './database/database.module';
 import { TrendsModule } from 'trends/trends.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -14,6 +15,24 @@ import { TrendsModule } from 'trends/trends.module';
       isGlobal: true, 
       envFilePath: '.env',
     }),
+
+    // BullMQ Redis 연결 기본 설정
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST') || 'localhost',
+          port: configService.get<number>('REDIS_PORT') || 6379,
+          password: configService.get<string>('REDIS_PASSWORD'),
+        },
+      }),
+    }),
+    // 트렌드 수집용 큐 등록
+    BullModule.registerQueue({
+      name: 'trend-scraper-queue',
+    }),
+
     // 10초에 10번으로 요청 제한
     ThrottlerModule.forRoot([
       {
